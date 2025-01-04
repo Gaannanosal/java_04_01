5525ca8506bf2063b174ba2d245cb1c15891caaa
Your Name3 <you@example.com>
2025-01-04 20:44:18 +0100

    js1

---
+++ script.js
@@ -0,0 +1,25 @@
+
+  let num =  prompt("введите число ")
+  console.log(num * 0.1)
+  let first = prompt ("Введите первое число")
+ let second = prompt ("Введите второе число")
+if (first > second){
+     console.log(second);
+    
+ }
+ else {console.log(first);
+ }
+
+ let num1 =  prompt("введите число ")
+ if (num1  <100){ console.log(`Число меньше 100`)
+
+ } else if(num1>100){console.log("Число больше 100");
+ }
+ else {console.log("Число равно 100");
+ }
+let userName = prompt ("Введите ваше имя.") 
+let userAge = prompt ("Введите ваш возраст.")
+if (userAge <18) {console.log("Hi");
+}
+else {console.log('Hallo,'+" "+ userName);
+}
